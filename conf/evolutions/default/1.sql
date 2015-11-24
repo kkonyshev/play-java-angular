ba2191edb3a2cee7ceef2dd5a6cf74dba219202e
Your Name <you@example.com>
2015-11-24 22:45:49 +0600

    User, Seller, SellPoint and tests -- evolution

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,66 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table sell_point (
+  id                        bigint not null,
+  name                      varchar(255),
+  seller_id                 bigint,
+  constraint pk_sell_point primary key (id))
+;
+
+create table seller (
+  id                        bigint not null,
+  name                      varchar(255),
+  constraint pk_seller primary key (id))
+;
+
+create table user (
+  email                     varchar(255) not null,
+  name                      varchar(255),
+  password                  varchar(255),
+  constraint pk_user primary key (email))
+;
+
+
+create table seller_sell_point (
+  seller_id                      bigint not null,
+  sell_point_id                  bigint not null,
+  constraint pk_seller_sell_point primary key (seller_id, sell_point_id))
+;
+create sequence sell_point_seq;
+
+create sequence seller_seq;
+
+create sequence user_seq;
+
+alter table sell_point add constraint fk_sell_point_seller_1 foreign key (seller_id) references seller (id) on delete restrict on update restrict;
+create index ix_sell_point_seller_1 on sell_point (seller_id);
+
+
+
+alter table seller_sell_point add constraint fk_seller_sell_point_seller_01 foreign key (seller_id) references seller (id) on delete restrict on update restrict;
+
+alter table seller_sell_point add constraint fk_seller_sell_point_sell_poi_02 foreign key (sell_point_id) references sell_point (id) on delete restrict on update restrict;
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists sell_point;
+
+drop table if exists seller;
+
+drop table if exists seller_sell_point;
+
+drop table if exists user;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists sell_point_seq;
+
+drop sequence if exists seller_seq;
+
+drop sequence if exists user_seq;
+
